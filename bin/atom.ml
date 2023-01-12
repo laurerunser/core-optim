36e97327609b5240e4af3d5367276b4e6e539dcd
Zhang Maryline <maryline.zhang@hotmail.com>
2023-01-12 13:45:13 +0100

    Fix typo

--- bin/atom.ml
+++ bin/atom.ml
@@ -1,10 +1,10 @@
 type t = {
-  indentifier : string; [@equal fun _ _ -> true] [@compare fun _ _ -> 0]
+  identifier : string; [@equal fun _ _ -> true] [@compare fun _ _ -> 0]
   number : int;
 }
 [@@deriving show, eq, ord]
 
-let pretty_print_atom a = Format.asprintf "%s__%d" a.indentifier a.number
+let pretty_print_atom a = Format.asprintf "%s__%d" a.identifier a.number
 
 let number =
   let c = ref 0 in
@@ -12,4 +12,4 @@ let number =
     incr c;
     !c
 
-let fresh ident = { indentifier = ident; number = number () }
+let fresh ident = { identifier = ident; number = number () }
