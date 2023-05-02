84fd447a58b74608aaca3690905e357c8a53de07
laure <laure.runser@etu.u-paris.fr>
2023-05-02 15:48:42 +0200

    add assertions to make sure invariants are satisfied

--- bin/stack.ml
+++ bin/stack.ml
@@ -48,19 +48,6 @@ let well_scoped (t : 'a scoped) (freevars_term : 'a -> VarSet.t)
       VarSet.subset f_term term_key && VarSet.subset f_ty ty_key
 
 let well_scoped_term t = well_scoped t free_vars free_ty_vars_of_term
-(* let well_scoped_base b = well_scoped b free_vars_base (fun _ -> VarSet.empty)
-   let well_scoped_ty ty = well_scoped ty (fun _ -> VarSet.empty) free_ty_vars *)
-
-(* let rec well_scoped_stack (s : stack) =
-   match s with
-   | [] -> true
-   | HoleFun a :: s ->
-       if not (well_scoped_base a) then false else well_scoped_stack s
-   | HoleType t :: s ->
-       if not (well_scoped_ty t) then false else well_scoped_stack s
-   | HoleIf (e1, e2) :: s ->
-       if not (well_scoped_term e1 || well_scoped_term e2) then false
-       else well_scoped_stack s *)
 
 (* Returns a scoped term where the scope is [t], and all the sets/maps are empty *)
 let empty_scope t =
@@ -264,7 +251,7 @@ and go (t : term scoped) (acc : stack) =
     | TypeApply (f, ty), acc (*@ \label{go:typeApply}*) ->
         let f = inherit_scope ~x:f ~scope:t in
         let ty = inherit_scope ~x:ty ~scope:t in
-        go f (HoleType ty (***) :: acc)
+        go f (HoleType ty :: acc)
     | IfThenElse (t1, t2, t3), acc (*@ \label{go:ite}*) ->
         let t1 = inherit_scope ~x:t1 ~scope:t in
         let t2 = inherit_scope ~x:t2 ~scope:t in
